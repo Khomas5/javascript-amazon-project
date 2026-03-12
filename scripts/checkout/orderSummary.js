219db1b8b313e00562374adb8af3012423796f52
Khomas5 <ana.khomasuridze.2@iliauni.edu.ge>
2026-03-12 14:02:08 +0400

    Save data and generate HTML for payment summery

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,9 +5,9 @@ import {
   updateQuantity,
   updateDeliveryOption
 } from "../../data/cart.js"
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 
 //default export
 //import dayjs from "https://unpkg.com/dayjs@1.11.10/dayjs.min.js";
@@ -22,23 +22,11 @@ let cartSummaryHTML = "";
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
 
-  let matchingProduct;
-
-  products.forEach((product) => {
-    if (product.id === productId) {
-      matchingProduct = product;
-    }
-  });
+  const matchingProduct = getProduct(productId);
 
   const deliveryOptionId = cartItem.deliveryOptionId;
 
-  let deliveryOption = deliveryOptions[0];
-
-  deliveryOptions.forEach((option) => {
-    if (option.id === deliveryOptionId) {
-      deliveryOption = option;
-    }
-  });
+  const deliveryOption = getDeliveryOption(deliveryOptionId);
 
   const today = dayjs();
   const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
