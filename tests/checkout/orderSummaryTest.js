406fcd3fe663853f3a5fad589b748d17bacf3c8b
Khomas5 <ana.khomasuridze.2@iliauni.edu.ge>
2026-03-17 23:09:02 +0400

    Load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,9 +1,17 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { cart } from "../../data/cart-class.js";
+import { loadProducts } from "../../data/products.js";
 
 describe("test suite: renderOrderSummary", () => {
-       const productsId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-    const productsId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+  const productsId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productsId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+
+  beforeAll((done) => { 
+  loadProducts(() => {
+    done();
+  });
+
+  });
 
   beforeEach(() => {
     spyOn(localStorage, "setItem");
@@ -15,16 +23,19 @@ describe("test suite: renderOrderSummary", () => {
    <div class="js-payment-summery"></div>
    
         `;
-    
-        cart.cartItems = [{
-          productId: productsId1,
-          quantity: 2, 
-          deliveryOptionId: '1'
-        },{
-          productId: productsId2,
-          quantity: 1, 
-          deliveryOptionId: '2'
-        }]
+
+    cart.cartItems = [
+      {
+        productId: productsId1,
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: productsId2,
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
 
     renderOrderSummary();
   });
@@ -57,11 +68,8 @@ describe("test suite: renderOrderSummary", () => {
       document.querySelector(`.js-cart-item-container-${productsId2}`),
     ).not.toEqual(null);
 
- 
     expect(cart.cartItems.length).toEqual(1);
     expect(cart.cartItems[0].productId).toEqual(productsId2);
-
-
 
     document.querySelector(".js-test-container").innerHTML = "";
   });
