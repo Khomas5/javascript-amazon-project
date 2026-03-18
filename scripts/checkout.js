c4a32b220514620d56aca1298d1e7d9fd88036f4
Khomas5 <ana.khomasuridze.2@iliauni.edu.ge>
2026-03-18 15:04:31 +0400

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,12 +2,33 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadCart } from "../data/cart.js";
-import { loadProductsFetch} from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-import { loadProducts } from "../data/products.js";
+async function loadPage() {
+  try {
+    // throw 'error1';
+    await loadProductsFetch();
 
+    const value = await new Promise((resolve, reject) => {
+      //  throw 'error2';
+
+      loadCart(() => {
+        // reject('error3')
+        resolve("value3");
+      });
+    });
+  } catch (error) {
+    console.log("Uexpected error. Please try again later");
+  }
+
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -21,7 +42,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
-
+*/
 /*
 new Promise((resolve) => {
   loadProducts(() => {
