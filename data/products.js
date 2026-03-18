ef1418a67063419e862c076f535e43437aa822ae
Khomas5 <ana.khomasuridze.2@iliauni.edu.ge>
2026-03-18 12:18:46 +0400

    Convert loadProducts from XHR request

--- data/products.js
+++ data/products.js
@@ -108,29 +108,54 @@ object3.method();
 */
 export let products = [];
 
+export function loadProductsFetch() {
+ const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productData) => {
+      products = productData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        } else if (productDetails.type === "appliance") {
+          return new Appliance(productDetails);
+        }
+
+        return new Product(productDetails);
+      });
+
+      console.log("load products");
+
+    });
+    return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
   xhr.addEventListener("load", () => {
-  products = JSON.parse(xhr.response).map((productDetails) => {
-  if(productDetails.type === 'clothing'){
-   return new Clothing(productDetails);
-  } else if (productDetails.type === 'appliance'){
-    return new Appliance(productDetails);
-  }
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === "appliance") {
+        return new Appliance(productDetails);
+      }
 
- return new Product(productDetails);
-});
+      return new Product(productDetails);
+    });
 
-console.log('load products')
+    console.log("load products");
 
-fun();
+    fun();
   });
 
   xhr.open("GET", "https://supersimplebackend.dev/products");
   xhr.send();
 }
-
 
 /*
 export const products = [
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,17 +2,14 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadCart } from "../data/cart.js";
+import { loadProductsFetch} from "../data/products.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
 import { loadProducts } from "../data/products.js";
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve("value1");
-    });
-  }),
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
